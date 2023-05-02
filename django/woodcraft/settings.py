5fba01d7f72e5e560c98e0c4c8eef339074cc5c6
idonacmias <idonacmi@gmail.com>
2023-05-02 21:07:46 +0300

    add CORS_ALLOWED_ORIGINS of new netlify server to django setting

--- django/woodcraft/settings.py
+++ django/woodcraft/settings.py
@@ -186,7 +186,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'https://master--thriving-salamander-68aff4.netlify.app']
 
 
 #relevent only if i will use session else can be deletet
